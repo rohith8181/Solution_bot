bfd92ea86fd20353dfa08f836d3a20762e5c8ae0
rohith8181 <80666597+rohith8181@users.noreply.github.com>
2024-03-16 19:16:35 +0530

    Update UserSchema.js

--- Controllers/schema/UserSchema.js
+++ Controllers/schema/UserSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     AccountId: { type: Number, required: true },
     user_name: { type: String, default: 'bot_user' },
-    Credits: { type: Number, default: 1 },
+    Credits: { type: Number, default: 10 },
     AnswerFormat: { type: String, default: "text" },
 })
 
